43e5f9980c36d00bb815a13e8b2e32488d7bce44
niedzii <niedzwiad14@gmail.com>
2021-06-26 16:30:22 +0200

    Revert "changed from wimlt to mad"
    This reverts commit 1bc99c36cf7f301c7e3d4805140c5fe652511cab.

---
+++ precache-manifest.a6dfc9c42fdd03a98d5dce8fd51ed3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4874c3a7985b3f96c4b99656c14e55f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849372435749ed617f29",
+    "url": "/static/css/main.afdbf793.chunk.css"
+  },
+  {
+    "revision": "8e4763adb4d8c96e3bc0",
+    "url": "/static/js/2.b96f785f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b96f785f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849372435749ed617f29",
+    "url": "/static/js/main.820893f1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
